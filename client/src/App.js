a38fee8d19fea9e058691beed18644ef6344c5d6
mks-94 <muralikrishna9424.com>
2020-12-06 18:50:37 +0530

    Implemented shopping list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import AppNavbar from "./components/AppNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import ShoppingList from "./components/ShoppingList";
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
