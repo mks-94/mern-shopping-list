401f308b351c1eb0d7df840bfe4bef9f347e704f
mks-94 <muralikrishna9424.com>
2020-12-13 11:58:48 +0530

    Implemented redux into client and it works fine.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
+import React from "react";
+import { Container } from "reactstrap";
 import AppNavbar from "./components/AppNavbar";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import ItemModal from "./components/itemModal";
 import ShoppingList from "./components/ShoppingList";
 
-function App() {
-  return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
-  );
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
